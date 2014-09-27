787104ff570064d872da951c3b0186cc71d1ed2e
Mihai Dinca <dincamihai@gmail.com>
2014-09-27 19:23:42 +0300

    support commplex numbers up to 6 digits

--- constants.py
+++ constants.py
@@ -1,41 +1,46 @@
 import string
 
 
-NUMBERS = dict(
-    zip(
-        string.digits[1:],
-        [
-            'one',
-            'two',
-            'three',
-            'four',
-            'five',
-            'six',
-            'seven',
-            'eight',
-            'nine'
-        ]
-    )
-)
+NUMBERS = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+}
+
 
 for key, value in NUMBERS.items():
-    NUMBERS[unicode(int(key)+10)] = '%steen' % value
-    NUMBERS[unicode(int(key)*10)] = '%sty' % value
+    NUMBERS[key+10] = '%steen' % value
+    NUMBERS[key*10] = '%sty' % value
+    NUMBERS[key*100] = '%s hundred' % value
+
 
 NUMBERS.update(dict([
-    ('11', 'eleven'),
-    ('12', 'twelve'),
-    ('13', 'thirteen'),
-    ('15', 'fifteen'),
-    ('18', 'eighteen'),
+    (11, 'eleven'),
+    (12, 'twelve'),
+    (13, 'thirteen'),
+    (15, 'fifteen'),
+    (18, 'eighteen'),
 
 ]))
 
+
 NUMBERS.update(dict([
-    ('0', 'zero'),
-    ('10', 'ten'),
-    ('20', 'twenty'),
-    ('30', 'thirty'),
-    ('50', 'fifty'),
-    ('80', 'eighty'),
+    (10, 'ten'),
+    (20, 'twenty'),
+    (30, 'thirty'),
+    (50, 'fifty'),
+    (80, 'eighty'),
 ]))
+
+
+for key, value in NUMBERS.items():
+    NUMBERS[key*1000] = '%s thousand' % value
+
+
+NUMBERS[0] = 'zero'
--- exercises.py
+++ exercises.py
@@ -1,31 +1,38 @@
 #!/usr/bin/env python
 
+import pytest
 from constants import NUMBERS
+
+
+MIN = 0
+MAX = 999999
 
 
 class BadInputValueException(Exception):
     """ raised when value is not in the allowed interval """
 
+    message = 'value needs to be an integer between %s and %s' % (MIN, MAX)
+
 
 class NumberConverter(object):
 
-    def __init__(self, value):
-        if not 0 <= value <= 1000:
-            message = 'value needs to be an integer between 0 and 1000'
-            raise BadInputValueException, message
 
-        self.value = unicode(value)
+    def __init__(self, value):
+        if not MIN <= value <= MAX:
+            raise BadInputValueException
+
+        self.value = value
 
     def in_words(self):
         words = []
         if self.value in NUMBERS:
             return NUMBERS[self.value]
         else:
-            digits = [d for d in self.value]
+            digits = [d for d in unicode(self.value)]
             for idx, digit_value in enumerate(digits, start=1):
-                digit_value = unicode(
-                    int(digit_value) * pow(10, len(digits)-idx)
-                )
+                if digit_value == '0':
+                    continue
+                digit_value = int(digit_value) * pow(10, len(digits)-idx)
                 words.append(NUMBERS[digit_value])
         return ' '.join(words)
 
@@ -39,36 +46,89 @@ def convert(value):
 
 def test_negative_1():
     output = convert(-1)
-    assert output == 'value needs to be an integer between 0 and 1000'
+    assert output == BadInputValueException.message
 
-def test_1001():
-    output = convert(1001)
-    assert output == 'value needs to be an integer between 0 and 1000'
 
 def test_a():
     output = convert('not a number')
-    assert output == 'value needs to be an integer between 0 and 1000'
+    assert output == BadInputValueException.message
 
-def test_0():
-    assert 'zero' == convert(0)
+
+def test_MIN():
+    assert 'zero' == convert(MIN)
+
+
+@pytest.mark.parametrize('number', xrange(MIN, 21))
+def test_all_simple_numbers(number):
+    assert number in NUMBERS
+
+
+@pytest.mark.parametrize('number', xrange(1, 10))
+def test_2_digits_multiples_of_10(number):
+    assert number*10 in NUMBERS
+
+
+@pytest.mark.parametrize('number', xrange(1, 10))
+def test_3_digits_multiples_of_100(number):
+    assert number*100 in NUMBERS
+
+
+@pytest.mark.parametrize('number', xrange(1, 10))
+def test_4_digits_multiples_of_100(number):
+    assert number*1000 in NUMBERS
+
+
+@pytest.mark.parametrize('number', xrange(1, 10))
+def test_5_digits_multiples_of_100(number):
+    assert number*10000 in NUMBERS
+
+
+@pytest.mark.parametrize('number', xrange(1, 10))
+def test_6_digits_multiples_of_100(number):
+    assert number*100000 in NUMBERS
+
 
 def test_3():
     assert 'three' == convert(3)
 
+
 def test_12():
     assert 'twelve' == convert(12)
+
 
 def test_18():
     assert 'eighteen' == convert(18)
 
-def test_20():
-    assert 'twenty' == convert(20)
 
 def test_21():
     assert 'twenty one' == convert(21)
 
+
 def test_89():
     assert 'eighty nine' == convert(89)
 
-def test_90():
-    assert 'ninety' == convert(90)
+
+def test_355():
+    assert 'three hundred fifty five' == convert(355)
+
+
+def test_1001():
+    assert 'one thousand one' == convert(1001)
+
+
+def test_1355():
+    assert 'three hundred fifty five' == convert(355)
+
+
+def test_9999():
+    assert 'nine thousand nine hundred ninety nine' == convert(9999)
+
+
+@pytest.mark.xfail
+def test_99999():
+    assert 'ninety nine thousand nine hundred ninety nine' == convert(99999)
+
+
+@pytest.mark.xfail
+def test_MAX():
+    assert 'nine hundred ninety nine thousand nine hundred ninety nine' == convert(MAX)
